e00f0e9feb9d46dc28b676f3500a2c5c0b6c93c1
coholist <i@holi.st>
2023-05-13 13:45:11 +0000

    Add date sorting to graphs

--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,28 +1,53 @@
 import type { TUserStat } from '@/types/stats';
 import { useGraph } from '@/hooks/useGraph';
-import { AreaChart } from '@tremor/react';
+import {
+  AreaChart,
+  Button,
+  DateRangePicker,
+  DateRangePickerValue,
+} from '@tremor/react';
 
 export interface IGraph {
   stat: TUserStat;
 }
 
 export const Graph = ({ stat }: IGraph): JSX.Element => {
-  const { push, graph } = useGraph(stat);
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+  const todayEnd = new Date();
+  todayEnd.setHours(23, 59, 59);
+
+  const { push, graph, setRange } = useGraph(stat, [oneYearAgo, todayEnd]);
+
+  const handleRangeChange = (event: DateRangePickerValue) => {
+    const [from, to] = event;
+    if (!from || !to) return;
+    from.setHours(0, 0, 0);
+    to.setHours(23, 59, 59);
+    setRange([from, to]);
+  };
 
   return (
-    <div className="w-full block">
+    <div className="max-w-[400px] block p-2 flex flex-col gap-2">
       <AreaChart
         data={graph}
         index={stat.name}
         categories={[stat.name]}
+        showAnimation
+        curveType="natural"
       ></AreaChart>
-      <button
+      <DateRangePicker
+        maxDate={todayEnd}
+        minDate={oneYearAgo}
+        onValueChange={handleRangeChange}
+      ></DateRangePicker>
+      <Button
         onClick={() => {
           push(Math.random() * 256);
         }}
       >
         ADD
-      </button>
+      </Button>
     </div>
   );
 };
